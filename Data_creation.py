2487f4c87c04368c3ef1df862eb3a0ae47763a93
Ekaterina Levitskaya <43274336+el2727@users.noreply.github.com>
2018-11-20 00:14:46 -0500

    Update Data_creation.py

--- Data_creation.py
+++ Data_creation.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[39]:
-
-
 # Read in text files from folder 
 
 import glob
@@ -15,22 +12,13 @@ for name in files:
     with open(name) as f:
         content.append(f.readlines())
         
-
-
-# In[81]:
-
-
 # Get file names as target English names
 
 file_names = []
 for i in os.listdir('weighted/weighted/'):
     file_names.append(i)
 
-
-# In[128]:
-
-
-# Baliness pre-processing
+# Balinese pre-processing
 
 words_ban = []
 for i in content:
@@ -68,19 +56,11 @@ for i in zipped_ban:
     if i[0] != '-':
         full_pairs_ban.append(i)
 
-
-# In[137]:
-
-
 translit_file_ban = []
 for i in full_pairs_ban:
     translit_file_ban.append(i[0] + ' ' + i[1])
     
 len(translit_file_ban)
-
-
-# In[139]:
-
 
 # Train and test 80 and 20 
 
@@ -95,10 +75,6 @@ with open('test_ban.lex', 'w') as file:
     for i in test_ban:
         file.write(i + '\n')
 
-
-# In[41]:
-
-
 # Korean pre-processing
 
 words_kor = []
@@ -107,43 +83,22 @@ for i in content:
         if "kor_1985" in y:
             words_kor.append(y)
             
-
-
-# In[75]:
-
-
 words_kor_processed = []
 for i in words_kor:
     i = re.sub(r'kor_1985', '', i)
     words_kor_processed.append(i)
-
-
-# In[76]:
-
 
 words_kor_finalized = []
 for i in words_kor_processed:
     i = re.sub(r'\t', '', i)
     words_kor_finalized.append(i)
 
-
-# In[77]:
-
-
 words_kor_list = []
 for i in words_kor_finalized:
     i = re.sub(r'\n', '', i)
     words_kor_list.append(i)
 
-
-# In[106]:
-
-
 zipped_kor = list(zip(words_kor_list, file_names))
-
-
-# In[107]:
-
 
 full_pairs_kor = []
 for i in zipped_kor:
@@ -151,22 +106,12 @@ for i in zipped_kor:
         full_pairs_kor.append(i)
             
 
-
-# In[123]:
-
-
 translit_file_kor = []
 for i in full_pairs_kor:
     translit_file_kor.append(i[0] + ' ' + i[1])
 
 
-# In[124]:
-
-
 len(translit_file_kor)
-
-
-# In[140]:
 
 
 # Train and test 80 and 20 
@@ -183,9 +128,6 @@ with open('test_kor.lex', 'w') as file:
         file.write(i + '\n')
 
 
-# In[82]:
-
-
 # Russian pre-processing
 
 words_ru = []
@@ -194,65 +136,33 @@ for i in content:
         if "rus_centralasian" in y:
             words_ru.append(y)
 
-
-# In[83]:
-
-
 words_ru_processed = []
 for i in words_ru:
     i = re.sub(r'rus_centralasian', '', i)
     words_ru_processed.append(i)
-
-
-# In[84]:
-
 
 words_ru_finalized = []
 for i in words_ru_processed:
     i = re.sub(r'\t', '', i)
     words_ru_finalized.append(i)
 
-
-# In[85]:
-
-
 words_ru_list = []
 for i in words_ru_finalized:
     i = re.sub(r'\n', '', i)
     words_ru_list.append(i)
-
-
-# In[101]:
-
-
+    
 zipped_ru = list(zip(words_ru_list, file_names))
-
-
-# In[104]:
-
 
 full_pairs_ru = []
 for i in zipped_ru:
     if i[0] != '-':
         full_pairs_ru.append(i)
 
-
-# In[109]:
-
-
 translit_file_ru = []
 for i in full_pairs_ru:
     translit_file_ru.append(i[0] + ' ' + i[1])
 
-
-# In[114]:
-
-
 len(translit_file_ru)
-
-
-# In[116]:
-
 
 # Train and test 80 and 20 
 
@@ -260,15 +170,9 @@ train_ru = translit_file_ru[:461]
 test_ru = translit_file_ru[116:]
 
 
-# In[121]:
-
-
 with open('train_ru.lex', 'w') as file:
     for i in train_ru:
         file.write(i + '\n')
-
-
-# In[122]:
 
 
 with open('test_ru.lex', 'w') as file:
